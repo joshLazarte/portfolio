dfc40a0fee09e203323446c88441dca9b5652379
Josh <joshualazarte@yahoo.com>
2019-08-04 21:43:31 -0500

    add resume download route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express"),
-    app = express(),
-    bodyParser = require('body-parser');
+  app = express(),
+  bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -10,7 +10,9 @@ app.use(bodyParser.json());
 
 const landing = require("./routes/landing");
 const contact = require("./routes/contact");
+const downloads = require("./routes/downloads");
 app.use(landing);
-app.use('/contact', contact);
+app.use("/contact", contact);
+app.use("/downloads", downloads);
 
 app.listen(process.env.PORT, () => console.log("server started"));
---
+++ routes/downloads.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/resume", (req, res) => {
+  const file = path.join("./public/docs", "Josh-Lazarte-Resume.pdf");
+  res.download(file, err => {
+    if (err) {
+      console.log(err);
+      res.send("There was an error downloading the file");
+    }
+  });
+});
+
+module.exports = router;
