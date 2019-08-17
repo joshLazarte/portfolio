3102d51f894246075e602d8b0a999f6bd7e63f3d
Josh <joshualazarte@yahoo.com>
2019-08-16 21:32:52 -0500

    remove uneeded config files

--- config/messages.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-    res.locals.messages = require("express-messages")(req, res);
-    next();
-};
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 
 app.use(express.static('client/build'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +10,7 @@ app.use('/contact', require('./routes/contact'));
 app.use('/downloads', require('./routes/downloads'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('./client/build/index.html'));
+  res.sendFile('./client/build/index.html');
 });
 
 app.listen(process.env.PORT, () => console.log('server started'));
