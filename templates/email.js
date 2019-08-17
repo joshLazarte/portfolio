0c71c137304d8248fbce9df9c4f16f72e6895dec
Josh <joshualazarte@yahoo.com>
2019-08-17 15:19:06 -0500

    change mesage body name

--- templates/email.js
+++ templates/email.js
@@ -1,10 +1,10 @@
-const getEmailTemplate = (data) => (
-    `<b>New Message From Website</b>
+const getEmailTemplate = data =>
+  `<b>New Message From Website</b>
 <h3>Info:</h3>
 <ul>
     <li>Name: ${data.name}</li>
     <li>Email: ${data.email}</li>
-    <li>Message: ${data.message}</li>
-</ul>`);
+    <li>Message: ${data.body}</li>
+</ul>`;
 
 module.exports = getEmailTemplate;
