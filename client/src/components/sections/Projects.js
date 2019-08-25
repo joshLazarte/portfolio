9f14c8cffea96fae08e3b002987e0b22a76a831d
Josh <joshualazarte@yahoo.com>
2019-08-25 17:22:41 -0500

    add projects.json

--- client/src/components/sections/Projects.js
+++ client/src/components/sections/Projects.js
@@ -1,42 +1,5 @@
 import React from 'react';
-
-const projects = [
-  {
-    _id: 1,
-    title: 'Tower Tracker',
-    href: '/tower-tracker',
-    img: '/images/projects/tower-tracker.PNG',
-    alt: 'Cell Tower Tracker',
-    description:
-      'Enterprise application for maintaining and locating cell towers'
-  },
-  {
-    _id: 2,
-    title: 'MERN Calendar',
-    href: '/calendar-app',
-    img: '/images/projects/calendar.PNG',
-    alt: 'Calendar of events',
-    githubLink: 'https://github.com/joshLazarte/calendar_app',
-    description: 'Create, manage, and share events. Built with the MERN stack.'
-  },
-  {
-    _id: 3,
-    title: 'Budget App',
-    href: '/budget-app',
-    img: '/images/projects/budget-capture.PNG',
-    alt: 'Budget dashboard',
-    githubLink: 'https://github.com/joshLazarte/budget-tool',
-    description: 'Simple budget tool built with vanilla javascript and Chart.js'
-  },
-  {
-    _id: 4,
-    title: 'Community of Faith',
-    href: 'https://wearecommunityoffaith.org',
-    img: '/images/projects/cof.PNG',
-    alt: 'Community of Faith church website',
-    description: 'Local church website 100% custom designed and hand coded'
-  }
-];
+import projects from '../../assets/projects/projects.json';
 
 const Projects = () => {
   return (
